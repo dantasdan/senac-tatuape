7e9a822dde5847bf65492f195c3f8913cf72566a
Dantas Daniel <daniel.sidrim3@gmail.com>
2025-09-12 13:37:53 -0300

    Add files via upload

---
+++ UC1/03-EletronicaBasicaArduino/piscaLED/piscaLED.ino
@@ -0,0 +1,15 @@
+/**
+ Pisca LED. Aula 1
+ @author Daniel Dantas (github.com/dantasdan)
+*/
+
+void setup() {
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(13, HIGH);
+  delay(1000);
+  digitalWrite(13, LOW);
+  delay(1000);
+}
